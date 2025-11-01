c4622828b922f1c60d5c688342e6aa8a3f617ff6
samah hamed <samahhamed@samahs-MacBook-Pro.local>
2025-11-01 19:54:57 +0200

    remove the separator from the end of the text

--- week3/day1/index.js
+++ week3/day1/index.js
@@ -1,18 +1,20 @@
 let count = 0
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
-
-function increment(){
+let firstEntry = true
+function increment() {
     count = count + 1
     countEl.textContent = count
 }
 
-function resetCounter(){
+function resetCounter() {
     count = 0
     countEl.textContent = count
 }
 
-function save(){
-    saveEl.textContent += count + " - "
+function save() {
+    const separator = firstEntry ? "" : " - "
+    saveEl.textContent += separator + count
+    firstEntry = false
     resetCounter()
 }
